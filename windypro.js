8312482caba802ed0952a6790ea3083573281dc7
redskunk1231 <58417167+redskunk1231@users.noreply.github.com>
2026-08-31 13:45:15 +0800

    Update and rename windypro.conf to windypro.js

---
+++ windypro.js
@@ -0,0 +1,13 @@
+#!name=WindyPro 解锁
+#!desc=解锁 WindyPro 订阅功能
+
+
+[rewrite_local]
+
+^https?:\/\/account\.windy\.com\/api\/info url script-response-body WindyProCrack.js
+
+[mitm]
+
+hostname = account.windy.com
+
+var ddgksf2013=JSON.parse($response.body);$done({body:JSON.stringify(ddgksf2013={真实会员信息})});
